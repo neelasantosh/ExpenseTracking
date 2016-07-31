d580dba4a971a9ca190b5b319fdb6bea5fcf5e70
neelasantosh <neela.santosh2405@gmail.com>
2016-07-31 16:12:22 +0530

    Expense

---
+++ Expense/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Expense
+//
+//  Created by Rajesh on 18/12/15.
+//  Copyright © 2015 CDAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Expense/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Expense
+//
+//  Created by Rajesh on 18/12/15.
+//  Copyright © 2015 CDAC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
